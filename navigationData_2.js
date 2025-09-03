e1db1dfc14aaee7a9a6883d7f2b4fb3da8784b6e
murphy4321 <mike@kloa.com>
2025-09-03 13:05:39 -0700

    Add files via upload

---
+++ navigationData_2.js
@@ -0,0 +1,143 @@
+windows.customMenus = [
+  {
+    "id": "Northeast",
+    "title": "Northeast",
+    "data": [
+      {
+        "label": "Maine",
+        "items": [
+          {
+            "label": "District 1",
+            "items": [
+              {
+                "label": "District Info",
+                "url": "https://duckduckgo.com"
+              },
+              {
+                "label": "Candidates",
+                "items": [
+                  {
+                    "label": "Cecilia Perez",
+                    "url": "https://duckduckgo.com"
+                  },
+                  {
+                    "label": "Mitzi Rollins",
+                    "url": "https://duckduckgo.com"
+                  },
+                  {
+                    "label": "Roland Frank",
+                    "url": "https://duckduckgo.com"
+                  }
+                ]
+              }
+            ]
+          },
+          {
+            "label": "District 2",
+            "items": [
+              {
+                "label": "District Info",
+                "url": "https://duckduckgo.com"
+              },
+              {
+                "label": "Candidates",
+                "items": [
+                  {
+                    "label": "Cecilia Perez",
+                    "url": "https://duckduckgo.com"
+                  },
+                  {
+                    "label": "Mitzi Rollins",
+                    "url": "https://duckduckgo.com"
+                  },
+                  {
+                    "label": "Roland Frank",
+                    "url": "https://duckduckgo.com"
+                  }
+                ]
+              }
+            ]
+          },
+          {
+            "label": "District 3",
+            "items": [
+              {
+                "label": "District Info",
+                "url": "https://duckduckgo.com"
+              },
+              {
+                "label": "Candidates",
+                "items": [
+                  {
+                    "label": "Cecilia Perez",
+                    "url": "https://duckduckgo.com"
+                  },
+                  {
+                    "label": "Mitzi Rollins",
+                    "url": "https://duckduckgo.com"
+                  },
+                  {
+                    "label": "Roland Frank",
+                    "url": "https://duckduckgo.com"
+                  }
+                ]
+              }
+            ]
+          },
+          {
+            "label": "District 4",
+            "items": [
+              {
+                "label": "District Info",
+                "url": "https://duckduckgo.com"
+              },
+              {
+                "label": "Candidates",
+                "items": [
+                  {
+                    "label": "Cecilia Perez",
+                    "url": "https://duckduckgo.com"
+                  },
+                  {
+                    "label": "Mitzi Rollins",
+                    "url": "https://duckduckgo.com"
+                  },
+                  {
+                    "label": "Roland Frank",
+                    "url": "https://duckduckgo.com"
+                  }
+                ]
+              }
+            ]
+          },
+          {
+            "label": "District 5",
+            "items": [
+              {
+                "label": "District Info",
+                "url": "https://duckduckgo.com"
+              },
+              {
+                "label": "Candidates",
+                "items": [
+                  {
+                    "label": "Cecilia Perez",
+                    "url": "https://duckduckgo.com"
+                  },
+                  {
+                    "label": "Mitzi Rollins",
+                    "url": "https://duckduckgo.com"
+                  },
+                  {
+                    "label": "Roland Frank",
+                    "url": "https://duckduckgo.com"
+                  }
+                ]
+              }
+            ]
+          }
+        ]
+      }
+    ]
+  }
+];
